2297bcc08434e851ed408f16ce8f1dba9417cb60
pooya pooya <40429062-hoshang6710@users.noreply.replit.com>
2025-03-13 08:16:30 +0000

    Update PORT

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import time
 import threading
 from flask import Flask, jsonify
 from bot import start_bot
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ Bot is running!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 # تنظیم لاگ‌ها
 logging.basicConfig(level=logging.INFO)
